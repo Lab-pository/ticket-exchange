ff8a71b8108d3294eb643c248af68f8d313643ab
SeoSeunghun <proto_seo@naver.com>
2023-05-16 10:48:17 +0900

    init: ticket exchange project

---
+++ src/main/java/com/ticketexchange/TicketExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.ticketexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketexchange/TicketExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
