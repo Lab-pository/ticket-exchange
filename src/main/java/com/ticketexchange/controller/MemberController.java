eec94a84718eb9b67f771f31d9e038940e67ea64
SeoSeunghun <proto_seo@naver.com>
2023-05-20 00:31:29 +0900

    feat: 이메일, 닉네임 중복 검사 API 추가

--- src/main/java/com/ticketexchange/controller/MemberController.java
+++ src/main/java/com/ticketexchange/controller/MemberController.java
@@ -2,10 +2,15 @@ package com.ticketexchange.controller;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ticketexchange.controller.dto.LoginRequest;
@@ -18,6 +23,8 @@ import com.ticketexchange.support.web.ApiResult;
 @RestController
 @RequestMapping("/api/v1")
 public class MemberController {
+
+	private final Logger log = LoggerFactory.getLogger(getClass());
 	private final MemberService memberService;
 
 	public MemberController(MemberService memberService) {
@@ -38,5 +45,21 @@ public class MemberController {
 	public ResponseEntity<ApiResult<LoginResponse>> login(@RequestBody LoginRequest loginRequest) {
 		LoginResponse loginResponse = LoginResponse.of(memberService.login(loginRequest.toLoginDto()));
 		return ResponseEntity.ok(ApiResult.succeed(loginResponse));
+	}
+
+	@GetMapping("/members/email")
+	public ResponseEntity<Void> duplicateEmail(@RequestParam String email) {
+		if (memberService.isDuplicateEmail(email)) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+	@GetMapping("/members/nickname")
+	public ResponseEntity<Void> duplicateNickname(@RequestParam String nickname) {
+		if (memberService.isDuplicateNickname(nickname)) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
