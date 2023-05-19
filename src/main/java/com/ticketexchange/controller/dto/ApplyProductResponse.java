26a78f548c3116e5069ea0c8ac2040e9da0f69fc
SeoSeunghun <proto_seo@naver.com>
2023-05-20 00:29:02 +0900

    feat: controller dto 수정

--- src/main/java/com/ticketexchange/controller/dto/ApplyProductResponse.java
+++ src/main/java/com/ticketexchange/controller/dto/ApplyProductResponse.java
@@ -4,19 +4,26 @@ import com.ticketexchange.service.dto.ApplyProductDto;
 
 public class ApplyProductResponse {
 	private boolean result;
+	private ProductResponse product;
 
 	protected ApplyProductResponse() {
 	}
 
-	public ApplyProductResponse(boolean result) {
+	public ApplyProductResponse(boolean result, ProductResponse product) {
 		this.result = result;
+		this.product = product;
 	}
 
 	public static ApplyProductResponse of(ApplyProductDto applyProductDto) {
-		return new ApplyProductResponse(applyProductDto.isResult());
+		return new ApplyProductResponse(applyProductDto.isResult(),
+			ProductResponse.of(applyProductDto.getProductDto()));
 	}
 
 	public boolean isResult() {
 		return result;
+	}
+
+	public ProductResponse getProduct() {
+		return product;
 	}
 }
---
+++ src/main/java/com/ticketexchange/controller/dto/EarnedProductResponse.java
@@ -0,0 +1,43 @@
+package com.ticketexchange.controller.dto;
+
+import java.time.LocalDateTime;
+
+import com.ticketexchange.service.dto.EarnedProductDto;
+
+public class EarnedProductResponse {
+	private Long earnedProductId;
+	private Long productId;
+	private String name;
+	private LocalDateTime createdAt;
+
+	protected EarnedProductResponse() {
+	}
+
+	public EarnedProductResponse(Long earnedProductId, Long productId, String name, LocalDateTime createdAt) {
+		this.earnedProductId = earnedProductId;
+		this.productId = productId;
+		this.name = name;
+		this.createdAt = createdAt;
+	}
+
+	public static EarnedProductResponse of(EarnedProductDto earnedProductDto) {
+		return new EarnedProductResponse(earnedProductDto.getEarnedProductId(), earnedProductDto.getProductId(),
+			earnedProductDto.getName(), earnedProductDto.getCreatedAt());
+	}
+
+	public Long getEarnedProductId() {
+		return earnedProductId;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+}
