654508ffa0725440127a62a7fe26d7f3c69e7a5f
SeoSeunghun <proto_seo@naver.com>
2023-05-20 00:29:16 +0900

    feat: service dto 수정

--- src/main/java/com/ticketexchange/service/dto/ApplyProductDto.java
+++ src/main/java/com/ticketexchange/service/dto/ApplyProductDto.java
@@ -1,16 +1,28 @@
 package com.ticketexchange.service.dto;
 
+import com.ticketexchange.domain.Product;
+
 public class ApplyProductDto {
 	private boolean result;
+	private ProductDto productDto;
 
 	protected ApplyProductDto() {
 	}
 
-	public ApplyProductDto(boolean result) {
+	public ApplyProductDto(boolean result, ProductDto productDto) {
 		this.result = result;
+		this.productDto = productDto;
+	}
+
+	public static ApplyProductDto of(boolean result, Product product) {
+		return new ApplyProductDto(result, ProductDto.of(product));
 	}
 
 	public boolean isResult() {
 		return result;
+	}
+
+	public ProductDto getProductDto() {
+		return productDto;
 	}
 }
