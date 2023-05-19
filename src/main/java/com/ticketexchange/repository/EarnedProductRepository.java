5446490fabb5d5265b5ddc2406ec2a19c835872b
SeoSeunghun <proto_seo@naver.com>
2023-05-20 00:33:20 +0900

    feat: 멤버 아이디로 획득 상품 확인 함수 추가 및 N + 1문제 해결

--- src/main/java/com/ticketexchange/repository/EarnedProductRepository.java
+++ src/main/java/com/ticketexchange/repository/EarnedProductRepository.java
@@ -1,8 +1,13 @@
 package com.ticketexchange.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ticketexchange.domain.EarnedProduct;
 
 public interface EarnedProductRepository extends JpaRepository<EarnedProduct, Long> {
+	@EntityGraph(attributePaths = { "product" })
+	List<EarnedProduct> findAllByAcquiredById(Long acquiredById);
 }
