b05738a3bdd0c36c4647794d25d27ad12fd33613
SeoSeunghun <proto_seo@naver.com>
2023-05-20 00:29:33 +0900

    feat: Health Controller 구현

---
+++ src/main/java/com/ticketexchange/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.ticketexchange.controller;
+
+import org.slf4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ticketexchange.support.web.ApiResult;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class HealthController {
+
+	private final Logger log = org.slf4j.LoggerFactory.getLogger(getClass());
+
+	@GetMapping
+	public ResponseEntity<ApiResult<String>> health() {
+		log.info("health check");
+		return ResponseEntity.ok(ApiResult.succeed("OK"));
+	}
+}
