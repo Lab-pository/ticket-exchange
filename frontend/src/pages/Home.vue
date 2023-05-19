254ae2a73ea933f1351472540a6ab6d6ced9b658
SeoSeunghun <proto_seo@naver.com>
2023-05-19 23:52:33 +0900

    feat: Home에서 product불러오도록 구현

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,12 +1,35 @@
 <template>
-  <Header/>
-
+  <div class="home">
+    <div class="container">
+      <div class="home-intro">
+        <h1>뽑기 연습</h1>
+        <p>뽑기 연습을 해보세요.</p>
+      </div>
+      <div class="products-container">
+        <div v-for="(p, idx) in state.products" :key="idx">
+          <Product :product="p"></Product>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Home"
-}
+<script setup>
+import axios from "axios";
+import {onMounted, reactive} from "vue";
+import lib from "@/scripts/lib";
+import Product from "@/components/Product.vue";
+
+const state = reactive({
+  products: []
+});
+
+const now = lib.getNowDateWithFormat();
+onMounted(async () => {
+  await axios.get("/api/v1/products", {params: {now: now}}).then((res) => {
+    state.products = res.data.data;
+  })
+});
 </script>
 
 <style scoped>
