8da9681f9482546d06a17a96afc956e30069439f
SeoSeunghun <proto_seo@naver.com>
2023-12-05 17:31:43 +0900

    refactor: 사용하지 않는 코드 제거

--- src/test/java/com/ticketexchange/fixture/MemberFixture.java
+++ src/test/java/com/ticketexchange/fixture/MemberFixture.java
@@ -1,17 +1,10 @@
 package com.ticketexchange.fixture;
 
-import java.time.Instant;
-import java.util.Base64;
-
-import com.ticketexchange.auth.MemberToken;
 import com.ticketexchange.controller.dto.LoginRequest;
 import com.ticketexchange.controller.dto.MemberRequest;
-import com.ticketexchange.utils.JsonUtils;
 
 public final class MemberFixture {
 
-    public static final Long MEMBER_ID = 1L;
-    public static final Long TOKEN_EXPIRE = 1000L * 60 * 60 * 24 * 7;
     public static final String EMAIL = "protoseo@naver.com";
     public static final String NICKNAME = "protoseo";
     public static final String PASSWORD = "password";
@@ -33,10 +26,5 @@ public final class MemberFixture {
 
     public static MemberRequest 회원가입_요청(final String email, final String nickname, final String password) {
         return new MemberRequest(email, nickname, password);
-    }
-
-    public static String 회원토큰() {
-        String token = JsonUtils.toJson(new MemberToken(MEMBER_ID, Instant.now().toEpochMilli() + TOKEN_EXPIRE));
-        return Base64.getEncoder().encodeToString(token.getBytes());
     }
 }
