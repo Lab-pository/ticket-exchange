f3897b2b172dece1cb2a7d63cbeec806d01e6f4a
SeoSeunghun <proto_seo@naver.com>
2023-12-08 22:32:12 +0900

    feat: 테스트 픽스쳐 추가

--- src/test/java/com/ticketexchange/fixture/MemberFixture.java
+++ src/test/java/com/ticketexchange/fixture/MemberFixture.java
@@ -1,10 +1,20 @@
 package com.ticketexchange.fixture;
 
+import java.time.Instant;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.ticketexchange.auth.MemberToken;
 import com.ticketexchange.controller.dto.LoginRequest;
 import com.ticketexchange.controller.dto.MemberRequest;
+import com.ticketexchange.domain.Member;
+import com.ticketexchange.service.dto.CreateMemberDto;
+import com.ticketexchange.service.dto.LoginDto;
 
 public final class MemberFixture {
 
+    public static final Long TOKEN_EXPIRE = 1000L * 60 * 60 * 24 * 7;
+    public static final Long MEMBER_ID = 1L;
     public static final String EMAIL = "protoseo@naver.com";
     public static final String NICKNAME = "protoseo";
     public static final String PASSWORD = "password";
@@ -26,5 +36,27 @@ public final class MemberFixture {
 
     public static MemberRequest 회원가입_요청(final String email, final String nickname, final String password) {
         return new MemberRequest(email, nickname, password);
+    }
+
+    public static CreateMemberDto 회원가입_DTO() {
+        return new CreateMemberDto(EMAIL, NICKNAME, PASSWORD);
+    }
+
+    public static LoginDto 로그인_DTO() {
+        return 로그인_DTO(EMAIL, PASSWORD);
+    }
+
+    public static LoginDto 로그인_DTO(final String email, final String password) {
+        return new LoginDto(email, password);
+    }
+
+    public static MemberToken 회원토큰() {
+        return new MemberToken(MEMBER_ID, Instant.now().toEpochMilli() + TOKEN_EXPIRE);
+    }
+
+    public static Member 회원() {
+        final Member member = new Member(EMAIL, NICKNAME, PASSWORD);
+        ReflectionTestUtils.setField(member, "id", MEMBER_ID);
+        return member;
     }
 }
