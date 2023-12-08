66dbcc6c3e4b0a10f90ec6035d4a0065b2f123ca
SeoSeunghun <proto_seo@naver.com>
2023-12-08 22:34:08 +0900

    test: 객체 비교 테스트 추가

--- src/test/java/com/ticketexchange/domain/vo/ProbabilityTest.java
+++ src/test/java/com/ticketexchange/domain/vo/ProbabilityTest.java
@@ -1,5 +1,6 @@
 package com.ticketexchange.domain.vo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -27,5 +28,21 @@ class ProbabilityTest {
     void 확률이_1초과인_경우_예외를_던진다() {
         assertThatThrownBy(() -> new Probability(1.00001))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 확률_값이_다른_경우_다르다() {
+        final double 값 = 0.5;
+        final Probability 확률 = new Probability(값);
+
+        assertThat(new Probability(0.0)).isNotEqualTo(확률);
+    }
+
+    @Test
+    void 확률_값이_같은_경우_같다() {
+        final double 값 = 0.5;
+        final Probability 확률 = new Probability(값);
+
+        assertThat(new Probability(값)).isEqualTo(확률);
     }
 }
--- src/test/java/com/ticketexchange/domain/vo/ValidDateTest.java
+++ src/test/java/com/ticketexchange/domain/vo/ValidDateTest.java
@@ -1,7 +1,9 @@
 package com.ticketexchange.domain.vo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.time.LocalDate;
 
@@ -32,5 +34,29 @@ class ValidDateTest {
         final var 종료일 = LocalDate.of(2023, 1, 2);
 
         assertThatCode(() -> new ValidDate(시작일, 종료일)).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 시작일과_종료일이_모두_같은경우_같은_객체이다() {
+        final var 시작일 = LocalDate.of(2023, 1, 1);
+        final var 종료일 = LocalDate.of(2023, 1, 2);
+        final ValidDate 유효_응모일 = new ValidDate(시작일, 종료일);
+
+        assertThat(new ValidDate(시작일, 종료일)).isEqualTo(유효_응모일);
+    }
+
+    @Test
+    void 시작일과_종료일이_하나라도_다른경우_다른_객체이다() {
+        final var 시작일 = LocalDate.of(2023, 1, 1);
+        final var 종료일 = LocalDate.of(2023, 1, 2);
+        final var 다른_시작일 = LocalDate.of(2022, 1, 1);
+        final var 다른_종료일 = LocalDate.of(2024, 1, 2);
+        final ValidDate 유효_응모일 = new ValidDate(시작일, 종료일);
+
+        assertAll(() -> {
+            assertThat(new ValidDate(시작일, 다른_종료일)).isNotEqualTo(유효_응모일);
+            assertThat(new ValidDate(다른_시작일, 종료일)).isNotEqualTo(유효_응모일);
+            assertThat(new ValidDate(다른_시작일, 다른_종료일)).isNotEqualTo(유효_응모일);
+        });
     }
 }
